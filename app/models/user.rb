8de64371bf0ab0fbd1d0fcd342597c226273c497
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-17 15:24:18 -0500

    Create successful login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,12 @@
 class User < ApplicationRecord
-
-  attr_reader :password
-
-  validates :email, :password_digest, :session_token, presence: true
-  validates :email, uniqueness: true
-  validates :password, length: { minimum: 6 }, allow_nil: true
-
-  after_initialize :ensure_session_token
-
-  has_many :reviews,
-    foreign_key: :author_id
     
-  has_many :favorites
-  has_many :favorite_benches,
-    through: :favorites,
-    source: :bench
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    
+    after_initialize :ensure_session_token
+    
+    attr_reader :password
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
