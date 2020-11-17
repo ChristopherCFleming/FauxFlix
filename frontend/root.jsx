630e4ac03ec4a37f738d8ece2dfd6f44dd13a074
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-17 10:38:26 -0500

    Prepare for another Heroku push

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -4,12 +4,12 @@ import { HashRouter } from 'react-router-dom';
 
 import App from './app';
 
-const Root = ({ store }) => (
-    <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </Provider>
-);
+// const Root = ({ store }) => (
+//     <Provider store={store}>
+//         <HashRouter>
+//             <App />
+//         </HashRouter>
+//     </Provider>
+// );
 
-export default Root;
+// export default Root;
