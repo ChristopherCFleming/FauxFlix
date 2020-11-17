3913431e8ce4158eba56f0f58e8701d884ca0a6b
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-17 12:00:31 -0500

    Testing h1 change

--- frontend/fauxflix.jsx
+++ frontend/fauxflix.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const title = <h1>Faux Flix</h1>
+    const title = <h1>Faux AUTH TEST Flix</h1>
     ReactDOM.render(title, root);
 });
