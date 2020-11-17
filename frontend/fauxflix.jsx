857a5aa9d8a789c9d264b2a24416f7626c698822
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-17 13:58:42 -0500

    Create user backend

--- frontend/fauxflix.jsx
+++ frontend/fauxflix.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const title = <h1>Faux Auth Branch Flix</h1>
+    const title = <h1>Faux Flix</h1>
     ReactDOM.render(title, root);
 });
