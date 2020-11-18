5173ba931c69c7c8b6a95e46288c35c44288110a
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-18 10:58:04 -0500

    Maybe resolve conflict

--- frontend/fauxflix.jsx
+++ frontend/fauxflix.jsx
@@ -5,15 +5,6 @@ import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-<<<<<<< HEAD
-    const title = <h1>Faux AUTH TEST Flix</h1>
-    ReactDOM.render(title, root);
-=======
     const store = configureStore();
     ReactDOM.render(<Root store={store}/>, root);
-<<<<<<< HEAD
->>>>>>> auth
 });
-=======
-});
->>>>>>> auth
