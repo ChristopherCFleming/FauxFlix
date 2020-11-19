e0b21b9d84b94179e8a0a2cab68bb4f088aec087
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-19 10:25:16 -0500

    Render sign in, log in, and create user pages

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -2,9 +2,12 @@ import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
+    let nextState = Object.assign({}, state)
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+            // return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+            nextState[action.currentUser.id] = action.currentUser
+            return nextState;
         default:
             return state;
     }
