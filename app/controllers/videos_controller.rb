522105d08fae575cf5b560ef1400fc341f070654
ChristopherCFleming <chris.charnquist@gmail.com>
2020-11-23 15:30:00 -0500

    Seed database with sample video clips.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,11 @@ class VideosController < ApplicationController
         video = Video.new(video_params)
     end
 
+    def show
+        @post = Post.find(params[:id])
+        render :show
+    end
+
     def video_params(params)
         params.require(:video).permit(:title, :description, :genre_id)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   
+  resource :videos, only: [:show]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resource :users, only: [:create, :show]
     resource :session, only: [:create, :destroy, :show]
+    
   end
+
+  
 
   root 'static_pages#root'
 end
