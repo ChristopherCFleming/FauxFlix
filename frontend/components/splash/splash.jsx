0409e0a98d91d907014030559be6b4a9ec819268
ChristopherCFleming <chris.charnquist@gmail.com>
2021-01-27 13:01:21 -0500

    Change classes to className

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -66,7 +66,7 @@ class SplashPage extends React.Component {
                                 <div className="accordion">
                                     <button type="button" className="status accordion__button">What is Netflix?
                                         <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                        <div class="accordion__content">
+                                        <div className="accordion__content">
                                             <p>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. <br /> <br />You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!</p>
                                         </div>
                                     </button>
@@ -76,7 +76,7 @@ class SplashPage extends React.Component {
                                 <div className="accordion">
                                     <button type="button" className="status accordion__button">How much does Netflix cost?
                                         <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                        <div class="accordion__content">
+                                        <div className="accordion__content">
                                             <p>Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from $8.99 to $17.99 a month. No extra costs, no contracts.</p>
                                         </div>
                                     </button>
@@ -86,7 +86,7 @@ class SplashPage extends React.Component {
                                 <div className="accordion">
                                     <button type="button" className="status accordion__button">Where can I watch?
                                         <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                        <div class="accordion__content">
+                                        <div className="accordion__content">
                                             <p>Watch anywhere, anytime, on an unlimited number of devices. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. <br /> <br />You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.</p>
                                         </div>
                                     </button>
@@ -96,7 +96,7 @@ class SplashPage extends React.Component {
                                 <div className="accordion">
                                     <button type="button" className="status accordion__button">How do I cancel?
                                         <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                        <div class="accordion__content">
+                                        <div className="accordion__content">
                                             <p>Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.</p>
                                         </div>
                                     </button>
@@ -106,7 +106,7 @@ class SplashPage extends React.Component {
                                 <div className="accordion">
                                     <button type="button" className="status accordion__button">What can I watch on Netflix?
                                         <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                        <div class="accordion__content">
+                                        <div className="accordion__content">
                                             <p>Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.</p>
                                         </div>
                                     </button>
@@ -136,6 +136,13 @@ class SplashPage extends React.Component {
                         </a>
                     </div>
                 </footer>
+                {/* <script>
+                    document.querySelectorAll('status').forEach(button => {
+                        button.addEventListener('click', () => {
+                            this.classList.toggle("status--active");
+                        });
+                    });
+                </script> */}
             </div>
         )
     }
