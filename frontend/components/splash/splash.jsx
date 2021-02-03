1b50264bc9b964f781d72d47d9cf748f4a4b0cda
ChristopherCFleming <chris.charnquist@gmail.com>
2021-02-03 16:49:11 -0500

    Playing with accordion

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -86,46 +86,46 @@ class SplashPage extends React.Component {
                                 <h1>Frequently Asked Questions</h1>
                                 <ul className="faqButtonContainer">
                                     <li>
-                                <div className="accordion">
-                                        <button type="button" className={`status${this.state.activeAccordion === 1 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(1)}>Who Made This Amazing Site?
-                                            <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                            <div className="accordion__content">
-                                                <p>I'm Chris. Nice to meet you. I'm a multifaceted software engineer with a marketing background and an expertise in JavaScript, React, Redux, Ruby On Rails. Please enjoy this pixel perfect streaming service clone. Find me in the footer links and let me know what you think!</p>
-                                            </div>
-                                        </button>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="accordion">
-                                        <button type="button" className={`status${this.state.activeAccordion === 2 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(2)}>Tell Me More!
-                                            <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                            <div className="accordion__content">
-                                                <p>I'm an avid traveller. I've found it's an excellent opportunity to welcome growth into your life, to see it from another perspective. Some highlights: searching for geishas in Kyoto, motorbiking in Hanoi, a semester in Berlin, eating goat spine in Shenyang, Charles Bridge at midnight in Prague, 3 years in Beijing, climbing Il Duomo in Florence, scuba diving in El Nido. There are so many adventures out there waiting to teach you something new, and I intend to learn as much as possible.<br/><br />In my spare time you might find me obsessing over tiny homes and lying to my mom about eating goat spine in Shenyang. She worries too much. Let's keep that bit between us.</p>
-                                            </div>
-                                        </button>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="accordion">
-                                        <button type="button" className={`status${this.state.activeAccordion === 3 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(3)}>Other Projects?
-                                            <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                            <div className="accordion__content">
-                                                <p>Get Gov is a pure JavaScript website that can connect you with your representatives at all levels of government.<br /><br />Pomegranate is a MERN web app that helps reduce food waste by finding recipes for you to try based on the ingredients already in your kitchen.<br/><br/>Feel free to check them out by navigating to my portfolio site in the footer below.</p>
-                                            </div>
-                                        </button>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div className="accordion">
-                                        <button type="button" className={`status${this.state.activeAccordion === 4 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(4)}>How Can I Hire This Developer?
-                                            <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
-                                            <div className="accordion__content">
-                                                <p>Thank you for asking! I would very much so like to hear from you. Please click on the links in the footer for my LinkedIn, Angellist, and personal portfolio site.</p>
-                                            </div>
-                                        </button>
-                                    </div>
-                                </li>
-                            </ul>
+                                        <div className="accordion">
+                                            <button type="button" className={`status${this.state.activeAccordion === 1 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(1)}>Who Made This Amazing Site?
+                                                <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
+                                                <div className="accordion__content">
+                                                    <p>I'm Chris. Nice to meet you. I'm a multifaceted software engineer with a marketing background and an expertise in JavaScript, React, Redux, Ruby On Rails. Please enjoy this pixel perfect streaming service clone. Find me in the footer links and let me know what you think!</p>
+                                                </div>
+                                            </button>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="accordion">
+                                            <button type="button" className={`status${this.state.activeAccordion === 2 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(2)}>Tell Me More!
+                                                <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
+                                                <div className="accordion__content">
+                                                    <p>I'm an avid traveller. I've found it's an excellent opportunity to welcome growth into your life, to see it from another perspective. Some highlights: searching for geishas in Kyoto, motorbiking in Hanoi, a semester in Berlin, eating goat spine in Shenyang, Charles Bridge at midnight in Prague, 3 years in Beijing, climbing Il Duomo in Florence, scuba diving in El Nido. There are so many adventures out there waiting to teach you something new, and I intend to learn as much as possible.<br/><br />In my spare time you might find me obsessing over tiny homes and lying to my mom about eating goat spine in Shenyang. She worries too much. Let's keep that bit between us.</p>
+                                                </div>
+                                            </button>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="accordion">
+                                            <button type="button" className={`status${this.state.activeAccordion === 3 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(3)}>Other Projects?
+                                                <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
+                                                <div className="accordion__content">
+                                                    <p>Get Gov is a pure JavaScript website that can connect you with your representatives at all levels of government.<br /><br />Pomegranate is a MERN web app that helps reduce food waste by finding recipes for you to try based on the ingredients already in your kitchen.<br/><br/>Feel free to check them out by navigating to my portfolio site in the footer below.</p>
+                                                </div>
+                                            </button>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="accordion">
+                                            <button type="button" className={`status${this.state.activeAccordion === 4 ? "--active" : ""} accordion__button`} onClick={this.handleAccordionClick(4)}>How Can I Hire This Developer?
+                                                <svg viewBox="0 0 26 26" className="svg-icon svg-icon-thin-x svg-closed" focusable="true"><path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path></svg>
+                                                <div className="accordion__content">
+                                                    <p>Thank you for asking! I would very much so like to hear from you. Please click on the links in the footer for my LinkedIn, Angellist, and personal portfolio site.</p>
+                                                </div>
+                                            </button>
+                                        </div>
+                                    </li>
+                                </ul>
                             <h4>Ready to watch? Enter your email to create or restart your membership.</h4>
                             <div className="emailInput">
                                 <input type="email" name="email" className="inputBar" value={this.state.email} placeholder="Email address" onChange={this.update("email")}></input>
