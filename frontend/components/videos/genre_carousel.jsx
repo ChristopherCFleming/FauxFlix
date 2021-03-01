fe5c71484637e52b4df9c4d8cd4780e5b34c362f
ChristopherCFleming <chris.charnquist@gmail.com>
2021-03-01 18:33:12 -0500

    Shuffle videos in carousel so they sshow up in different order when you refresh

--- frontend/components/videos/genre_carousel.jsx
+++ frontend/components/videos/genre_carousel.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 function GenreCarousel(props) {
-    
-    const testing = 35;
 
         if(!props.allVideoObjects) {
             return (null);
         } else {
             return (
                 <div className="carousel">
+                   
                     {/* {props.genre.video_ids.map( (video_id_in_genre, idx) =>  */}
                     {/* // <video muted={true} controls={false} autoPlay={false} loop={true} src={props.videos[video_id_in_genre].video} width="10%" className="carouselVideo" type="video/mp4"></video>)} */}
                     {props.genre.video_ids.map( (each_id, index) => console.log(props.allVideoObjects[each_id].video))}
