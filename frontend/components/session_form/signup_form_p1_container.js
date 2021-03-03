6a3cb455697be90fdf5dca073590a624114738eb
ChristopherCFleming <chris.charnquist@gmail.com>
2021-03-03 16:25:54 -0500

    Clean up imports from signup_form_container_p1

--- frontend/components/session_form/signup_form_p1_container.js
+++ frontend/components/session_form/signup_form_p1_container.js
@@ -1,7 +1,5 @@
 import { connect } from 'react-redux';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { signup } from '../../actions/session_actions';
 import SignupFormP1 from './signup_form_p1';
 
 const mapStateToProps = (state, ownProps) => {
