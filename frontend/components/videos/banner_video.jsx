f6a816f79191a31c96910943900ee3ea5c415071
ChristopherCFleming <chris.charnquist@gmail.com>
2021-03-05 13:48:22 -0500

    Add play button to banner video

--- frontend/components/videos/banner_video.jsx
+++ frontend/components/videos/banner_video.jsx
@@ -11,7 +11,10 @@ function BannerVideo(props) {
                     <h1 className="bannerVideoTitle">{props.randVideo.title}</h1>
                     <p>Watch Now</p>
                     <h5 className="bannerVideoDescription">{props.randVideo.description}</h5>
+                    <div className="buttonsWrapper">
+                        <button className="playButton"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="playicon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 010 1.393z"></path></svg>Play</button>
 
+                    </div>
                 </div>
             </div>
         )
