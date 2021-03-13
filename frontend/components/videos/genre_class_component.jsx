538f3840aad9db0e0714028a86dc9bb052bf22e5
ChristopherCFleming <chris.charnquist@gmail.com>
2021-03-12 19:28:39 -0500

    Adjust carousel class component

--- frontend/components/videos/genre_class_component.jsx
+++ frontend/components/videos/genre_class_component.jsx
@@ -43,16 +43,14 @@ class GenreCarousel extends React.Component {
                 <div className="carousel" >
                     <p>{this.props.genre.genre}</p>
                     
-                    <Swiper loop={true} navigation slidesPerView={6}>
+                    <Swiper loop={true} navigation slidesPerView={6} className="swiperWrapper">
                         {this.shuffleVideos(this.props.genre.video_ids).map( (each_id, index) => (
                             <SwiperSlide className="slide" key={index} >
                                 <div className="videoSlideContainer" onMouseOver={this.mouseover}>
                                     <Link to={`/videos/${each_id}`}>
                                         <video 
                                             poster={this.props.allVideoObjects[each_id].thumbnail}
-                                            // poster={this.props.entities.videos[each_id].thumbnail} 
                                             src={this.props.allVideoObjects[each_id].video} 
-                                            // src={this.props.entities.videos[each_id].video} 
                                             className="carouselVideo" />
                                     </Link>
                                     <div className="videoDropDown">
@@ -60,10 +58,8 @@ class GenreCarousel extends React.Component {
                                             <span><Link to={`/videos/${each_id}`}><i className="far fa-play-circle"></i></Link></span>
                                             <span><i className="fas fa-plus-circle"></i></span>
                                             <h4>{this.props.allVideoObjects[each_id].title}</h4>
-                                            {/* <h4>{this.props.entities.videos[each_id].title}</h4> */}
                                         </div>
                                         <p>{this.shortenDescription(this.props.allVideoObjects[each_id].description)}</p>
-                                        {/* <p>{this.shortenDescription(this.props.entities.videos[each_id].description)}</p> */}
                                     </div>
                                 </div>
                             </SwiperSlide>
