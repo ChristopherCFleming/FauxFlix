168c04e4cea82341344ec0d5a87a312abbf11b28
ChristopherCFleming <chris.charnquist@gmail.com>
2021-03-19 17:51:30 -0400

    Adjust lists controller in backend to render json properly

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -6,18 +6,16 @@ class Api::ListsController < ApplicationController
             @lists = List.where(user_id: params[:user_id])
             render :index
        else
-        my_render(422, "Nothing to process!")
-        # render json: ["Nothing to process!"], status: 422
+        render json: ["Nothing Here"], status: 422
        end
     end
 
     def create 
-        @list = list.new(list_params)
+        @list = List.new(list_params)
         if @list.save!
             render :show
         else
-            my_render(422, @list.errors.full_messages)
-            # render json: @list.errors.full_messages, status: 422
+            render json: @list.errors.full_messages, status: 422
         end
     end
 
@@ -25,9 +23,11 @@ class Api::ListsController < ApplicationController
         
         @list = List.find_by(video_id: list_params[:video_id], user_id: list_params[:user_id])
         if List.destroy(@list.id)
-            my_render(200)
+            # Not sure about also adding the status below. Necessary?
+            render :show, status: 200
         else
-            my_render(404, @list.errors.full_messages)
+            # Just copied from line 18. Is correct?
+            render json: @list.errors.full_messages, status: 422
         end
     end
 
