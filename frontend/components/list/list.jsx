49ac6afe2843a2a73c545af0ee943320a8c90ca3
ChristopherCFleming <chris.charnquist@gmail.com>
2021-03-24 01:44:00 -0400

    Add boilerplate to list.jsx

---
+++ frontend/components/list/list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../reusable_components/header';
+import Footer from '../reusable_components/footer';
+
+
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+export default List;
