7f5ce0821905643a7334979b77a75adeaa5b9803
ChristopherCFleming <chris.charnquist@gmail.com>
2021-04-08 11:40:15 -0400

    Finish styling for search component and adjust for list component

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -24,7 +24,6 @@ class Search extends React.Component {
                 filtered.push(vids[i]);
             }
         }
-        debugger;
         this.setState({
             searchVideos: filtered
         });
@@ -48,17 +47,20 @@ class Search extends React.Component {
             return null;
         } else if (this.state.searchVideos.length === 0) {
             return (
-                <div className="listComponent empty">
+                <div className="emptySearch">
                     {/* Passing history as a prop to allow Navi the same react router ability*/}
                     <Navi loggedIn={true} history={this.props.history}/>
-                    <p>Your search for "{this.state.query}" returned no results.</p>
-                    <ul className="suggestionsList">Suggestions:
-                        <li className="suggestion">Search for a title</li>
-                        <li className="suggestion">Type "Sabrina" because The Chilling Adventures of Sabrina is an amazing series</li>
-                        <li className="suggestion">Hire me for introducing you to "The Chilling Adventures of Sabrina"</li>
-                        <li className="suggestion">If you don't like the series, hire me because you got a chuckle out of this</li>
-                        <li className="suggestion">In case none of the above apply: hire me anyways</li>
-                    </ul>
+                    <div className="textContainer">
+                        <h1 className="emptySearchTitle">Your search for "{this.state.query}" returned no results.</h1>
+                        <div className="suggestionsContainer">
+                            <ul className="suggestionsList">Suggestions:
+                                <li className="suggestion">Try keyword from title or description</li>
+                                <li className="suggestion">Type "Sabrina" (spectacular series!)</li>
+                                <li className="suggestion">Hire me for introducing you to "The Chilling Adventures of Sabrina"</li>
+                                <li className="suggestion">In case none of the above apply: hire me anyways</li>
+                            </ul>
+                        </div>
+                    </div>
                     <Footer />
                 </div>
             )
